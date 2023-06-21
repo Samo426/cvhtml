9dbcdc23c300eecfc185c8c77fe1aef7a37b3990
Samo426 <113669780+Samo426@users.noreply.github.com>
2023-06-21 09:05:57 +0200

    Add files via upload

---
+++ contact.form/ccontact.php
@@ -0,0 +1,79 @@
+<?php
+
+// configure
+$from = 'mail@mail.fr'; // TON MAIL d'envoie
+$sendTo = 'samuel42600@gmail.com'; // ton mail a toi
+$subject = 'Nouveau message du formulaire de contact ';
+$fields = array('name' => 'Name', 'email' => 'Email', 'subject' => 'Subject', 'message' => 'Message'); // array variable name => Text to appear in the email. If you added or deleted a field in the contact form, edit this array.
+$okMessage = ' <br> Le formulaire de contact a été soumis avec succès. Merci, je vous recontacterai bientôt ! <br>';
+$errorMessage = ' <br> Une erreur s\'est produite lors de la soumission du formulaire. Veuillez réessayer plus tard <br>';
+
+// let's do the sending
+
+if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])):
+    //your site secret key
+    $secret = 'ta clé secrete google pour le catptcha';
+    //get verify response data
+
+    $c = curl_init('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+    $verifyResponse = curl_exec($c);
+
+    $responseData = json_decode($verifyResponse);
+    if($responseData->success):
+
+        try
+        {
+            $emailText = nl2br("Vous avez un nouveau message du formulaire de contact\n");
+
+            foreach ($_POST as $key => $value) {
+
+                if (isset($fields[$key])) {
+                    $emailText .= nl2br("$fields[$key]: $value\n");
+                }
+            }
+
+            $headers = array('Content-Type: text/html; charset="UTF-8";',
+                'From: ' . $from,
+                'Reply-To: ' . $from,
+                'Return-Path: ' . $from,
+            );
+            
+            mail($sendTo, $subject, $emailText, implode("\n", $headers));
+
+            $responseArray = array('type' => 'success', 'message' => $okMessage);
+        }
+        catch (\Exception $e)
+        {
+            $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+        }
+
+        if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+            $encoded = json_encode($responseArray);
+
+            header('Content-Type: application/json');
+
+            echo $encoded;
+        }
+        else {
+            echo $responseArray['message'];
+        }
+
+    else:
+        $errorMessage = 'La vérification du robot a échoué, veuillez réessayer.';
+        $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+        $encoded = json_encode($responseArray);
+
+            header('Content-Type: application/json');
+
+            echo $encoded;
+    endif;
+else:
+    $errorMessage = 'Veuillez cliquer sur la case reCAPTCHA.';
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+    $encoded = json_encode($responseArray);
+
+            header('Content-Type: application/json');
+
+            echo $encoded;
+endif;
